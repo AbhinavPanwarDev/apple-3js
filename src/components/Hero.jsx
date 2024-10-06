5b5393c9e0b919f433fccb1670eebdbae3c0551f
Abhi <abhinav.panwarr@gmail.com>
2024-10-06 16:40:34 +0530

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,53 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
 
-  useGSAP( () => {
-    gsap.to('#hero', {opacity:1, delay:0.5})
-  }, [])
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo)
+    }else {
+      setVideoSrc(heroVideo)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet)
+
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet)
+    }
+  }, [])    // lets us play the selected video for the specific screen size with the resize effect using the useEffect
+
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay:1 });
+    gsap.to("#cta", { opacity: 1, y: -30, delay:2.5 });
+  }, []);
+
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">New iPhone 16 Pro Max</p>
+        <p id="hero" className="hero-title">iPhone 15 Pro Max</p>
+        <div className="md:w-10/12 w-9/12">
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
